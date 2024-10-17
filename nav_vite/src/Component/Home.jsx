f9f9aecad7d84afeec1270f211606d735471af0f
arjnramesh281 <arjunramesh281@gmail.com>
2024-10-17 11:27:52 +0530

    React Nav

--- nav_vite/src/Component/Home.jsx
+++ nav_vite/src/Component/Home.jsx
@@ -1,7 +1,43 @@
-function Home(){
-    return(
-        <h2>Home Page</h2>
-    )
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default Home
+const Home = () => {
+  const [product, setProduct] = useState([]);
+  const [add, setAdd] = useState([false]);
+  const [currentProduct, setCurrentProduct] = useState([null]);
+  useEffect(() => {
+    axios
+      .get("https://apiecommerce.pythonanywhere.com/api/product/")
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const view_p = (products) => {
+    setAdd(true);
+    setCurrentProduct(products);
+  };
+
+  return (
+    <div className="container mt-3">
+      <div className="row" id="a1">
+        {product.map((pro) => (
+          <div className="col-lg col-md-3 col-sm-6" key={pro.id}>
+            <div className="card" onClick={() => view_p(pro)}>
+              <img
+                className="card0-img-top img-fluid"
+                src={pro.image}
+                alt="product image"
+              />
+              <div className="card-body">
+                <h4 className="card-little">{pro.p_name}</h4>
+                <h6 className="card-little">{pro.price}</h6>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- nav_vite/src/Component/Layout.jsx
+++ nav_vite/src/Component/Layout.jsx
@@ -1,21 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 
 function Layout(){
     return(
-        <nav>
-            <ul>
-                <li>
-                   <Link to="/">Home</Link> 
-                </li>
-                <li>
-                <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </nav>
+    <>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
+    <div class="container">
+        <a class="navbar-brand me-5" href="javascript:void(0)">Myntra</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item me-2">
+            <Link className="text-light text-decoration-none" to="/">Home</Link> 
+            </li>
+            <li class="nav-item me-2">
+            <Link className="text-light text-decoration-none" to="/contact">Contact</Link>
+            </li>
+            <li class="nav-item me-2 ">
+            <Link className="text-light text-decoration-none" to="/about">About</Link>
+            </li>
+        </ul>
+        <form class="d-flex">
+            <input class="form-control me-2" type="text" placeholder="Search"/>
+            <button class="btn btn-primary" type="button">Search</button>
+        </form>
+        </div>
+    </div>
+    </nav>
+    </>
     )
 }
 
